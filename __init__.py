fac825abc18d0a7bac99433b032b5bac4a0f3a76
henridbr <henridbr@users.noreply.github.com>
2020-05-24 10:21:07 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -60,10 +60,10 @@ class NetatmoWeatherSkill(MycroftSkill):
             response = requests.post("https://api.netatmo.com/api/getstationsdata", params=params)
             response.raise_for_status()
             self.data = response.json()["body"]
-
+            print(self.data)
+            
         except requests.exceptions.HTTPError as error:
             print(error.response.status_code, error.response.text)
-        print(self.data)
         
 #### Intents
 # Conversation example
