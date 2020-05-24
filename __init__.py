66e6a009c319c0c97289d5ec6b3bba18021b3fc1
henridbr <henridbr@users.noreply.github.com>
2020-05-24 23:06:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -87,8 +87,8 @@ class NetatmoWeatherSkill(MycroftSkill):
     @intent_handler(IntentBuilder("NetatmoIntent").require("NetatmoKeyword"))
     @adds_context('NetatmoContext','netatmo')
     def handle_netatmo_intent(self, message):             
-            sta_name = self.data['devices'][0]['station_name']
-            self.speak_dialog('netatmo',{"sta_name":sta_name})        
+        sta_name = self.data['devices'][0]['station_name']
+        self.speak_dialog('netatmo',{"sta_name":sta_name})        
             
     @intent_handler(IntentBuilder("HomeTemperaturesIntent").require("HomeTemperaturesKeyword").require('NetatmoContext'))
     @adds_context('NetatmoContext','netatmo') 
